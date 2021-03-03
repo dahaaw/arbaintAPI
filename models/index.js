927bf706968b593b68edfa4b0f30426044082a7d
dahaaw <dahaaw@gmail.com>
2021-03-03 07:02:32 +0700

    sync model

--- models/index.js
+++ models/index.js
@@ -1,8 +1,10 @@
 const initModels = require('./init-models');
 var dbConnect = require('./../config/db.connect');
 const sequelize = dbConnect();
+sequelize.sync();
+console.log("All models were synchronized successfully.");
 
-const runModel = () => {
+const runModel = async () => {
     return initModels(sequelize);
 }
 
